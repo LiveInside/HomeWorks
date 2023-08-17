2f6e6d0824a2f9d248accc7ebb6eb252d49ff83d
LiveInside <maksyta053@mail.ru>
2023-08-17 15:50:04 +0300

    Add tests

--- src/main/java/org/homeworks/Main.java
+++ src/main/java/org/homeworks/Main.java
@@ -1,12 +1,6 @@
 package org.homeworks;
 
-import org.homeworks.collection.HomeWorkLists;
-
 public class Main {
     public static void main(final String[] args) {
-        System.out.println(HomeWorkLists.scrabble("rkqodlw", "world"));
-        System.out.println(HomeWorkLists.scrabble("ajv", "java"));
-        System.out.println(HomeWorkLists.scrabble("avjafff", "JaVa"));
-        System.out.println(HomeWorkLists.scrabble("", "hexlet"));
     }
 }
