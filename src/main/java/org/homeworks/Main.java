ad833d685ed6692aa4c6fbcea7ce7ed12f8d2c22
LiveInside <maksyta053@mail.ru>
2023-08-18 15:34:44 +0300

    Add tests

--- src/main/java/org/homeworks/Main.java
+++ src/main/java/org/homeworks/Main.java
@@ -6,9 +6,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(final String[] args) {
-        Map<String, Integer> wordsCount = HomeWorkMaps
-                .getWordCount("the Java is the best programming language java");
-        System.out.println(wordsCount.size());
-        System.out.println(HomeWorkMaps.toString(wordsCount));
+
     }
 }
--- src/test/java/TestHomeWorks.java
+++ src/test/java/TestHomeWorks.java
@@ -1,16 +1,64 @@
-import org.assertj.core.api.Assertions;
 import org.homeworks.collections.HomeWorkLists;
+import org.homeworks.collections.HomeWorkMaps;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class TestHomeWorks {
+    // Тесты HomeWorkLists
     @Test
-    public void testScrabble(){
-        Assertions.assertThat(HomeWorkLists.scrabble("rkqodlw", "world")).isTrue();
-        Assertions.assertThat(HomeWorkLists.scrabble("ajv", "java")).isFalse();
-        Assertions.assertThat(HomeWorkLists.scrabble("avjafff", "JaVa")).isTrue();
-        Assertions.assertThat(HomeWorkLists.scrabble("", "hexlet")).isFalse();
-        Assertions.assertThat(HomeWorkLists.scrabble("abcde", "ace")).isTrue();
+    public void testScrabble() {
+        assertThat(HomeWorkLists.scrabble("rkqodlw", "world")).isTrue();
+        assertThat(HomeWorkLists.scrabble("ajv", "java")).isFalse();
+        assertThat(HomeWorkLists.scrabble("avjafff", "JaVa")).isTrue();
+        assertThat(HomeWorkLists.scrabble("", "hexlet")).isFalse();
+        assertThat(HomeWorkLists.scrabble("abcde", "ace")).isTrue();
+    }
+    // Тесты HomeWorkMaps
+    @Test
+    public void testGetWordCount() {
+        Map<String, Integer> wordsCount = HomeWorkMaps
+                .getWordCount("The, Java is... the best /programming language java!");
+
+        assertThat(wordsCount).hasSize(6);
+        assertThat(wordsCount)
+                .containsKey("the")
+                .containsKey("java")
+                .containsKey("is")
+                .containsKey("best")
+                .containsKey("language")
+                .containsKey("programming");
+
+        assertThat(wordsCount.get("the")).isEqualTo(2);
+        assertThat(wordsCount.get("java")).isEqualTo(2);
+        assertThat(wordsCount.get("is")).isEqualTo(1);
+        assertThat(wordsCount.get("best")).isEqualTo(1);
+        assertThat(wordsCount.get("language")).isEqualTo(1);
+        assertThat(wordsCount.get("programming")).isEqualTo(1);
+    }
+    @Test
+    public void testToString() {
+        Map <String, Integer> emptyMap = new HashMap<>();
+
+        assertThat(HomeWorkMaps.toString(emptyMap)).isEqualTo("{\n}");
+
+        Map<String, Integer> wordsCount = HomeWorkMaps
+                .getWordCount("The, Java is... the best /programming language java!");
+
+        assertThat(HomeWorkMaps.toString(wordsCount)).isEqualTo("""
+                {
+                  the: 2
+                  java: 2
+                  is: 1
+                  best: 1
+                  language: 1
+                  programming: 1
+                }""");
     }
 
 }
