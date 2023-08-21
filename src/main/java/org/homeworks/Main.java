29f07df7fb6379e09c742ae24bc486c8323808de
LiveInside <maksyta053@mail.ru>
2023-08-21 20:46:35 +0300

    Complete Homework "Lambdas"

--- src/main/java/org/homeworks/Main.java
+++ src/main/java/org/homeworks/Main.java
@@ -1,23 +1,35 @@
 package org.homeworks;
 
-import org.homeworks.collections.HomeWorkGenerics;
-import org.homeworks.collections.HomeWorkTests;
-import org.homeworks.collections.HomeWorksStreams;
+import org.homeworks.collections.HomeWorksLambdas;
 
 import java.util.*;
 
 public class Main {
     public static void main(final String[] args) {
-        String[] emails = {
-                "info@gmail.com",
-                "info@yandex.ru",
-                "mk@host.com",
-                "support@hexlet.io",
-                "info@hotmail.com",
-                "support.yandex.ru@host.com"
+        List<Map<String, String>> users = List.of(
+                Map.of("name", "Vladimir Nikolaev", "birthday", "1990-12-27", "gender", "male"),
+                Map.of("name", "Andrey Petrov", "birthday", "1989-11-23", "gender", "male"),
+                Map.of("name", "Anna Sidorova", "birthday", "1996-09-09", "gender", "female"),
+                Map.of("name", "John Smith", "birthday", "1989-03-11", "gender", "male"),
+                Map.of("name", "Vanessa Vulf", "birthday", "1985-11-16", "gender", "female"),
+                Map.of("name", "Alice Lucas", "birthday", "1986-01-01", "gender", "female"),
+                Map.of("name", "Elsa Oscar", "birthday", "1970-03-10", "gender", "female")
+        );
+        List<String> men = HomeWorksLambdas.takeOldestMans(users);
+        System.out.println(men);
+
+        String[][] images = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
         };
 
-        List<String> emailsList = Arrays.asList(emails);
-        System.out.println(HomeWorksStreams.getCountOfFreeEmails(emailsList));
+        String[][] enlargedImage = HomeWorksLambdas.enlargeArrayImage(images);
+
+        for (String[] row : enlargedImage) {
+            System.out.println(Arrays.toString(row));
+        }
+
     }
 }
---
+++ src/main/java/org/homeworks/collections/HomeWorksLambdas.java
@@ -0,0 +1,32 @@
+package org.homeworks.collections;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+public class HomeWorksLambdas {
+    public static List<String> takeOldestMans(List<Map<String, String>> users) {
+        return users.stream()
+                .filter(user -> user.get("gender").equals("male"))
+                .sorted(Comparator.comparing(user -> LocalDate.parse(user.get("birthday"))))
+                .map(user -> user.get("name"))
+                .collect(Collectors.toList());
+    }
+
+    public static String[][] enlargeArrayImage(String[][] image) {
+        int rows = image.length;
+        int cols = image[0].length;
+
+        return Arrays.stream(image)
+                .flatMap(row -> Stream.of(row, row))
+                .map(row -> Arrays.stream(row)
+                        .flatMap(pixel -> Stream.of(pixel, pixel))
+                        .toArray(String[]::new))
+                .toArray(String[][]::new);
+    }
+}
